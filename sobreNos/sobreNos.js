1f1c814bb224cd4a3a6c8bd7f279658d24b402e0
Bruno Daniel <104467144+brunoodaniel@users.noreply.github.com>
2023-10-16 18:38:16 -0300

    carousel funcionando

--- sobreNos/sobreNos.js
+++ sobreNos/sobreNos.js
@@ -1,12 +1,17 @@
-var tituloExterno = document.getElementById("tituloExterno");
-var subtituloExterno = document.getElementById("subtituloExterno");
-
-$('#carouselExampleIndicators').on('slid.bs.carousel', function () {
-    var activeIndex = $('#carouselExampleIndicators .carousel-item.active').index();
-    var titulo = $('#carouselExampleIndicators .carousel-item').eq(activeIndex).data('titulo');
-    var subtitulo = $('#carouselExampleIndicators .carousel-item').eq(activeIndex).data('subtitulo');
-
-    // Atualize o título e o subtítulo externos com base no índice atual do carrossel
-    tituloExterno.textContent = titulo;
-    subtituloExterno.textContent = subtitulo;
-});
+var frases = [
+    { titulo: "Título 1", subtitulo: "Subtítulo 1" },
+    { titulo: "Título 2", subtitulo: "Subtítulo 2" },
+    { titulo: "Título 3", subtitulo: "Subtítulo 3" }
+  ];
+  
+  $(document).ready(function(){
+    var tituloExterno = document.getElementById('tituloExterno');
+    var subtituloExterno = document.getElementById('subtituloExterno');
+  
+    $('#carouselExampleIndicators').on('slid.bs.carousel', function () {
+      var slideIndex = $('.carousel-item.active').index();
+      tituloExterno.textContent = frases[slideIndex].titulo;
+      subtituloExterno.textContent = frases[slideIndex].subtitulo;
+    });
+  });
+  
