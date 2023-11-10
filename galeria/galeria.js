0d7315d1003d753de0679612f62213c6ba482c0a
brunoodaniel <brunodanielsilva2002@gmail.com>
2023-11-10 12:32:03 -0300

    finalizando galeria e uns ajustes

---
+++ galeria/galeria.js
@@ -0,0 +1,21 @@
+function menuShow(){
+    let menuMobile = document.querySelector(".mobile-menu");
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    }
+    else{
+        menuMobile.classList.add('open');
+    }
+}
+
+document.querySelectorAll('.image-container img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+
+}
