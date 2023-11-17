d79c518ea39c338f1716a5b5e74a4a4778f1c6c7
brunoodaniel <brunodanielsilva2002@gmail.com>
2023-11-17 16:50:55 -0300

    finalizando agencias c

---
+++ agenciasCredenciadas/agenciasCredenciadas.js
@@ -0,0 +1,28 @@
+function menuShow(){
+    let menuMobile = document.querySelector(".mobile-menu");
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    }
+    else{
+        menuMobile.classList.add('open');
+    }
+}
+
+document.querySelectorAll('.image-container img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup-image').style.display = 'block';
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+
+}
+
+var path = "../eventos/eventos.html";
+document.querySelectorAll('ul li a').forEach(function(link) {
+  if (link.getAttribute('href') === path) {
+    link.classList.add('active');
+  }
+});
