072cf60aaddd155c0f5db3ad39635bd1bf8107c6
brunoodaniel <brunodanielsilva2002@gmail.com>
2023-11-22 14:22:54 -0300

    ajustes

--- sobreNos/sobreNos.js
+++ sobreNos/sobreNos.js
@@ -14,3 +14,20 @@ document.querySelectorAll('.navbar-list ul li a').forEach(function(link) {
     link.classList.add('active');
   }
 });
+
+
+
+var botao = document.getElementById('visitar-site-polvilho-ourense');
+
+  botao.addEventListener('click', function() {
+    window.open('https://ourensealimentos.com.br/', '_blank');
+});
+
+
+
+var botao = document.getElementById('visitar-site-polvilho-tupguar');
+
+  botao.addEventListener('click', function() {
+    window.open('https://tupguar.com.br/', '_blank');
+});
+
